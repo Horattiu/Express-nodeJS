d8465ccbed45b05bb9fd54f47da19de5fc821dc8
Horattiue <horatiu.oltn@gmail.comm>
2023-07-06 08:28:58 -0700

    port fix

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const blogRoutes = require("./routes/blogRoutes");
 // express app
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// const port = 3000;
 
 // connect to mondoDB
 
